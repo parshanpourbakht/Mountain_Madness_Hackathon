661115fa673d46eae19e7af6ba4c6c567ad236be
echanted <ejc12@sfu.ca>
2023-02-18 17:38:36 -0800

    Initial styling change with ChakraUI

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Text, Container, Stack } from "@chakra-ui/react";
 
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -26,58 +26,93 @@ function Home() {
     };
   
     return (
-      <Box maxW="lg" mx="auto" mt={10}>
-        <Box mb={8}>
-          <Heading as="h1" size="xl" textAlign="center">
-            What type of texter are you?
-          </Heading>
+      <>
+        <Container maxW={'3xl'}>
+          <Stack
+            as={Box}
+            textAlign={'center'}
+            spacing={{ base: 8, md: 14 }}
+            py={{ base: 20, md: 36 }}>
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+              lineHeight={'110%'}>
+              What Type of Texter<br />
+              <Text as={'span'} color={'green.400'}>
+                Are YOU?
+              </Text>
+            </Heading>
+          </Stack>
 
-          <Text mb={4} fontSize="lg">
-            Upload your Discord chat data and find out 
-            what type of texter you are! Simply drag and 
-            drop your CSV file into the box below or click 
-            to select a file.
-          </Text>
-            <Text fontWeight="bold" mb={4} fontSize="lg">How it works:</Text>
-            <Text mb={4} fontSize="lg">
-                Our app analyzes your chat data to identify your texting 
-                patterns and determine your unique texter type. Are you 
-                a long-winded conversationalist or a master of the 
+          <Text>
+              Upload your Discord chat data and find out
+              what type of texter you are! Simply drag and
+              drop your CSV file into the box below or click
+              to select a file.
+            </Text>
+            <Text fontWeight="bold" fontSize="lg">How it works:</Text>
+              <Text fontSize="lg">
+                Our app analyzes your chat data to identify your texting
+                patterns and determine your unique texter type. Are you
+                a long-winded conversationalist or a master of the
                 one-liner? Find out now!
-            </Text>
-        </Box>
-  
-        <Box
-          {...getRootProps()}
-          borderWidth={2}
-          borderStyle="dashed"
-          rounded="md"
-          p={4}
-          textAlign="center"
-          cursor="pointer"
-          _hover={{
-            bg: "gray.50",
-          }}
-          mb={8}
-        >
-          <input {...getInputProps()} />
-  
-          <Text fontSize="lg">
-            Drag and drop a CSV file here, or click to select a file
-          </Text>
-        </Box>
-  
-        {fileData && (
-          <Box mt={4}>
-            <Text fontSize="lg">
-              File selected: {fileData.name}
-            </Text>
-            <Button onClick={handleSubmit} mt={4}>
-              Submit
-            </Button>
+              </Text>
+        
+        <Box maxW="lg" mx="auto" mt={10}>
+            <Box mb={8}>
+              <Heading as="h1" size="xl" textAlign="center">
+                What type of texter are you?
+              </Heading>
+
+              <Text mb={4} fontSize="lg">
+                Upload your Discord chat data and find out
+                what type of texter you are! Simply drag and
+                drop your CSV file into the box below or click
+                to select a file.
+              </Text>
+              <Text fontWeight="bold" mb={4} fontSize="lg">How it works:</Text>
+              <Text mb={4} fontSize="lg">
+                Our app analyzes your chat data to identify your texting
+                patterns and determine your unique texter type. Are you
+                a long-winded conversationalist or a master of the
+                one-liner? Find out now!
+              </Text>
+              
+            </Box>
+
+            <Box
+              {...getRootProps()}
+              borderWidth={2}
+              borderStyle="dashed"
+              rounded="md"
+              p={4}
+              textAlign="center"
+              cursor="pointer"
+              _hover={{
+                bg: "gray.50",
+              }}
+              mb={8}
+            >
+              <input {...getInputProps()} />
+
+              <Text fontSize="lg">
+                Drag and drop a CSV file here, or click to select a file
+              </Text>
+            </Box>
+
+            {fileData && (
+              <Box mt={4}>
+                <Text fontSize="lg">
+                  File selected: {fileData.name}
+                </Text>
+                <Button onClick={handleSubmit} mt={4}>
+                  Submit
+                </Button>
+              </Box>
+            )}
           </Box>
-        )}
-      </Box>
+        </Container>
+      </>
     );
   }
   
